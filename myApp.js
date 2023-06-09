24a00cf4440af32fecf549b995e5ee5f0c04157a
Abdulraheem Ahmad <abdulahmad586@gmail.com>
2023-06-09 17:46:07 +0100

    Update myApp.js - Added time middleware

--- myApp.js
+++ myApp.js
@@ -16,6 +16,13 @@ app.get("/json", (req,res)=>{
   res.json({message: messageStyle == "uppercase" ? message.toUpperCase() : message });
 })
 
+app.get("/now",(req,res,next)=>{
+  req.time = (new Date()).toString();
+  next();
+}, (req,res)=>{
+  res.json({time: req.time });
+})
+
 function loggerMiddleWare(req,res,next){
   console.log(req.method+" "+req.path+" - "+req.ip)
   next()
