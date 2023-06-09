b0b7a6c6f5c5510bb1ef277408392370aa7f7fc9
Abdulraheem Ahmad <abdulahmad586@gmail.com>
2023-06-09 17:19:33 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,11 +1,11 @@
 let express = require('express');
 let app = express();
 
+app.use("/public", express.static(__dirname+"/public"));
+
 app.get("/", (req,res)=>{
   res.sendFile(__dirname+"/views/index.html");
 })
-
-
 
 
 
